950ad880cc61481c8025fb637a3d4ffa61a5828b
pvl <pav77788@gmail.com>
2020-09-22 17:30:40 +0300

    accelerometers almost done

--- Sensors/csvread.py
+++ Sensors/csvread.py
@@ -15,11 +15,11 @@ import sys
 import pickle
 
 
-
+"""
 file_path = filedialog.askopenfile(initialdir='/home/pvl/ROS/',
 title='Select telemetry'
 ).name #dialog window, for manual telemetry search
-
+"""
 
 
 df = pd.read_csv("/home/pvl/ROS/decor_result/ROS-arbeit/Sensors/2020.09.17_13.50.18_log.csv", delimiter=';', index_col=False, skiprows=1) # Read csv file
@@ -29,21 +29,39 @@ df = pd.read_csv("/home/pvl/ROS/decor_result/ROS-arbeit/Sensors/2020.09.17_13.50
 t   = df.iloc[:, 0].values.reshape(-1, 1) # Time, first collumn
 telem = df.iloc[:, 42].values.reshape(-1, 1)
 
+#Choosing only rows with the ID683D
 telem = pd.DataFrame(telem)
-
-
 telem.to_csv("/home/pvl/ROS/decor_result/ROS-arbeit/Sensors/ignore_1.csv", index=False)
-
-
-
 telem = pd.read_csv("/home/pvl/ROS/decor_result/ROS-arbeit/Sensors/ignore_1.csv", delimiter=' ', skiprows=1)
-
 telem1 = pd.DataFrame({'A': telem.iloc[:,3]})
-telem_ID =telem.loc[telem1['A'] == "68"]
+telem_ID = telem.loc[telem1['A'] == "68"]
+
+#accelerometer
+
+#x axis
+accx1 = telem_ID.iloc[:,5].astype(str)
+accx2 = telem_ID.iloc[:,6].astype(str)
+accx = accx2 + accx1
+
+#y axis
+accy1 = telem_ID.iloc[:,7].astype(str)
+accy2 = telem_ID.iloc[:,8].astype(str)
+accy = accy2 + accy1
+#z axis
+accz1 = telem_ID.iloc[:,9].astype(str)
+accz2 = telem_ID.iloc[:,10].astype(str)
+accz = accz2 + accz1
 
 
-#telem_ID.to_csv("/home/pvl/ROS/decor_result/ROS-arbeit/Sensors/ignore_2.csv", index=False)
-#print(telem)
+
+accx = accx.apply(lambda accx: int(accx, 16))
+accy = accy.apply(lambda accy: int(accy, 16))
+accz = accz.apply(lambda accz: int(accz, 16))
+
+accx = accx * 4 / 32768
+accy = accy * 4 / 32768
+accz = accz * 4 / 32768
+
 
 
 #convertation to human readable time
